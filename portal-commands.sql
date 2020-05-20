850f68c259660c5e45065efc0883a5a0f09b6326
Matthew Kweskin <mkweskin@users.noreply.github.com>
2020-05-20 12:03:51 -0400

    Update portal-commands.sql

--- portal-commands.sql
+++ portal-commands.sql
@@ -50,3 +50,79 @@ FROM surveys;
 SELECT year, month, day, ROUND(weight/1000, 2) AS weight_kg
 FROM surveys;
 
+-- Write a query that returns the plot_id, species_id, sex, and weight in mg.
+SELECT plot_id, species_id, sex, weight*1000
+FROM surveys;
+
+-- Ordering of results: ORDER BY
+SELECT plot_id, species_id, sex, weight*1000 AS weight_mg
+FROM surveys
+ORDER BY weight_mg;
+
+-- ORDER BY: Descending
+SELECT plot_id, species_id, sex, weight*1000 AS weight_mg
+FROM surveys
+ORDER BY weight_mg DESC;
+
+-- ORDER BY: multiple fields (DESC and ASC)
+
+SELECT plot_id, species_id, sex, weight*1000 AS weight_mg
+FROM surveys
+ORDER BY species_id ASC, weight_mg DESC;
+
+-- COUNT
+SELECT COUNT(*)
+FROM surveys;
+
+-- COUNT and summary
+SELECT COUNT(*), SUM(weight)
+FROM surveys;
+
+-- Write a query that returns the total weight, average weight,
+-- minimum and maximum weights for all animals caught over
+-- the duration of the survey.
+-- Can you modify it so that it calculates these values only for animals >= 5 and <10 grams?
+SELECT SUM(weight), AVG(weight), max(weight), min(weight)
+FROM surveys
+WHERE weight >=5 AND weight <10;
+
+-- GROUP BY
+SELECT species_id, SUM(weight), AVG(weight), max(weight), min(weight)
+FROM surveys
+GROUP BY species_id;
+
+-- HAVING
+SELECT species_id, SUM(weight), AVG(weight), max(weight), min(weight)
+FROM surveys
+GROUP BY species_id
+HAVING AVG(weight) >= 20;
+
+-- NULL values
+SELECT AVG(weight)
+FROM surveys;
+-- 42.67
+
+SELECT SUM(weight) / COUNT(*)
+FROM surveys;
+-- 38.75
+
+-- Exclude null values
+SELECT SUM(weight) / COUNT(*)
+FROM surveys
+WHERE weight IS NOT NULL;
+
+-- NULLS in WHERE !=
+SELECT COUNT(*)
+FROM surveys
+WHERE sex != 'F';
+-- 17348
+
+SELECT COUNT(*)
+FROM surveys
+WHERE sex = 'M';
+-- 17348
+
+SELECT COUNT(*)
+FROM surveys
+WHERE sex != 'F' OR sex IS NULL;
+-- 19859: includes NULL values
